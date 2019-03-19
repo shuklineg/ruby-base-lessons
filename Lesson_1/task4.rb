785a6107d915dcf39ccc4fb8119c6482608bb7cb
Evgenii Shuklin <shuklineg@gmail.com>
2019-03-19 18:14:47 +0500

    Lesson 1

--- Lesson_1/task4.rb
+++ Lesson_1/task4.rb
@@ -10,7 +10,7 @@ d = b**2 - 4*a*c
 if d < 0 
   puts "Корней нет"
 elsif d == 0
-  x = (-b + Math.sqrt(d))/(2*a)
+  x = -b/(2*a)
   puts "Дискриминант равен #{d}, корень уровнения #{x}"
 else
   x1 = (-b + Math.sqrt(d))/(2*a)
