da0706726c7e2e97dc61232019942c95367208ce
Evgenii Shuklin <shuklineg@gmail.com>
2019-03-19 16:13:51 +0500

    Lesson 1

---
+++ Lesson_1/task1.rb
@@ -0,0 +1,13 @@
+puts "Введите ваше имя"
+name = gets.strip
+
+puts "Введите ваш рост"
+height = gets.to_f
+
+weight = height-110
+
+if weight > 0
+  puts "#{name}, ваш идеальный вес #{weight}"
+else
+  puts "Ваш вес уже оптимальный"
+end
