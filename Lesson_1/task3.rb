ee2dc7c2e8358aeb4860bae71a531168d0fe7616
Evgenii Shuklin <shuklineg@gmail.com>
2019-03-19 16:13:51 +0500

    Lesson 1

---
+++ Lesson_1/task3.rb
@@ -0,0 +1,28 @@
+puts "Введите первую сторону треугольника"
+a=gets.to_f
+puts "Введите вторую сторону треугольника"
+b=gets.to_f
+puts "Введите третью сторону треугольника"
+c=gets.to_f
+
+if a > b and a > c
+  h = a
+  c1 = b
+  c2 = c
+elsif b > c and b > a
+  h = b
+  c1 = a
+  c2 = c
+else
+  h = c
+  c1 = a
+  c2 = b
+end 
+
+
+if h**2 == c1**2 + c2**2
+  puts "Треугольник прямоугольный"
+  puts "Треугольник равнобедренный" if c1 == c2
+elsif h == c1 and h == c2 and c1 == c2
+  puts "Треугольнки равносторонний"  
+end
